511ba7f1eed49523228b343b7cbf316c451d2359
root <skekf123@naver.com>
2019-12-19 21:47:04 +0900

    first

---
+++ 2_vars/value.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+// import "math"
+
+
+func main(){
+	const a int32 = 12
+	fmt.Println(a)
+}
---
+++ 2_vars/variable.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a string = "manjoong"
+	b := "manjoong"
+
+	var c int = 1
+	var d, e int = 2, 3
+
+	var f int
+	g := false
+	var h string
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d, e)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Println(h)
+
+
+}
